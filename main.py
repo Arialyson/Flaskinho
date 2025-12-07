abdace2a79ea9af17d217ca0e1d8c5c4f1af4907
Arialyson Lima <arialyson@gmail.com>
2025-12-07 08:56:21 -0300

    up

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Olá mundo!'
+    return render_template('index.html', Nome='Arialyson')
 
 if __name__ == '__main__':
-    app.run()
+    app.run() # (debug=True)
